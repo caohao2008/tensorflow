40c3594a495eeef38f028378bfd0ab84408b8d99
caohao <hit.caohao@gmail.com>
2017-01-11 06:36:41 +0000

    add comments for mnist

--- tensorflow/examples/tutorials/mnist/mnist_softmax.py
+++ tensorflow/examples/tutorials/mnist/mnist_softmax.py
@@ -14,7 +14,6 @@
 # ==============================================================================
 
 """A very simple MNIST classifier.
-
 See extensive documentation at
 http://tensorflow.org/tutorials/mnist/beginners/index.md
 """
@@ -34,17 +33,21 @@ FLAGS = None
 
 def main(_):
   # Import data
+  # about io , pls see doc: 
   mnist = input_data.read_data_sets(FLAGS.data_dir, one_hot=True)
 
   # Create the model
+  # feature x is 784 dimension
   x = tf.placeholder(tf.float32, [None, 784])
+  # weight is 784 * 10 array ( 10 class)
   W = tf.Variable(tf.zeros([784, 10]))
+  # b is bias for each class
   b = tf.Variable(tf.zeros([10]))
   y = tf.matmul(x, W) + b
 
   # Define loss and optimizer
   y_ = tf.placeholder(tf.float32, [None, 10])
-
+  print(mnist)
   # The raw formulation of cross-entropy,
   #
   #   tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(tf.nn.softmax(y)),
@@ -53,12 +56,13 @@ def main(_):
   # can be numerically unstable.
   #
   # So here we use tf.nn.softmax_cross_entropy_with_logits on the raw
-  # outputs of 'y', and then average across the batch.
+  # outputs of 'y', and then average across the batch. 
   cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(y, y_))
+  # for more about tf.nn function, pls see doc http://www.tensorfly.cn/tfdoc/api_docs/python/nn.html#embedding_lookup
   train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
 
   sess = tf.InteractiveSession()
-  tf.global_variables_initializer().run()
+  tf.initialize_all_variables().run()
   # Train
   for _ in range(1000):
     batch_xs, batch_ys = mnist.train.next_batch(100)
@@ -75,4 +79,4 @@ if __name__ == '__main__':
   parser.add_argument('--data_dir', type=str, default='/tmp/tensorflow/mnist/input_data',
                       help='Directory for storing input data')
   FLAGS, unparsed = parser.parse_known_args()
-  tf.app.run(main=main, argv=[sys.argv[0]] + unparsed)
+  tf.app.run(main=main)
