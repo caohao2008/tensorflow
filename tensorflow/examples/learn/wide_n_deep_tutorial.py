84b4b631ee2ec7c93662063ad23c5b93233a4212
caohao <hit.caohao@gmail.com>
2017-01-16 05:06:33 +0000

    make wide_and_deep demo runable

--- tensorflow/examples/learn/wide_n_deep_tutorial.py
+++ tensorflow/examples/learn/wide_n_deep_tutorial.py
@@ -127,6 +127,8 @@ def build_estimator(model_dir):
       capital_loss,
       hours_per_week,
   ]
+  #print("wide_columns =",wide_columns)
+  #print("deep_columns =",deep_columns)
 
   if FLAGS.model_type == "wide":
     m = tf.contrib.learn.LinearClassifier(model_dir=model_dir,
@@ -149,37 +151,47 @@ def input_fn(df):
   # Creates a dictionary mapping from each continuous feature column name (k) to
   # the values of that column stored in a constant Tensor.
   continuous_cols = {k: tf.constant(df[k].values) for k in CONTINUOUS_COLUMNS}
+  ##print("arrive here");
+  ##print("continuous_cols =",continuous_cols)
   # Creates a dictionary mapping from each categorical feature column name (k)
   # to the values of that column stored in a tf.SparseTensor.
   categorical_cols = {
       k: tf.SparseTensor(
           indices=[[i, 0] for i in range(df[k].size)],
           values=df[k].values,
-          dense_shape=[df[k].size, 1])
+          shape=[df[k].size, 1])
       for k in CATEGORICAL_COLUMNS}
+  ##print("categorical_cols =",categorical_cols)
   # Merges the two dictionaries into one.
   feature_cols = dict(continuous_cols)
   feature_cols.update(categorical_cols)
   # Converts the label column into a constant Tensor.
   label = tf.constant(df[LABEL_COLUMN].values)
+  
+  ##print("feature_cols =",feature_cols)
   # Returns the feature columns and the label.
   return feature_cols, label
+
 
 
 def train_and_eval():
   """Train and evaluate the model."""
   train_file_name, test_file_name = maybe_download()
+  ##print("train_file_name =",train_file_name)
+  ##print("test_file_name =",test_file_name)
   df_train = pd.read_csv(
       tf.gfile.Open(train_file_name),
       names=COLUMNS,
       skipinitialspace=True,
       engine="python")
+  ##print("df_train = ",df_train)
   df_test = pd.read_csv(
       tf.gfile.Open(test_file_name),
       names=COLUMNS,
       skipinitialspace=True,
       skiprows=1,
       engine="python")
+  ##print("df_test = ",df_test)
 
   # remove NaN elements
   df_train = df_train.dropna(how='any', axis=0)
@@ -191,13 +203,14 @@ def train_and_eval():
       df_test["income_bracket"].apply(lambda x: ">50K" in x)).astype(int)
 
   model_dir = tempfile.mkdtemp() if not FLAGS.model_dir else FLAGS.model_dir
-  print("model directory = %s" % model_dir)
+  ##print("model directory = %s" % model_dir)
 
   m = build_estimator(model_dir)
   m.fit(input_fn=lambda: input_fn(df_train), steps=FLAGS.train_steps)
   results = m.evaluate(input_fn=lambda: input_fn(df_test), steps=1)
   for key in sorted(results):
     print("%s: %s" % (key, results[key]))
+
 
 
 def main(_):
